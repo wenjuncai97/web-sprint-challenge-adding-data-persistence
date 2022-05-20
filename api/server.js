3af2dec158a5f5fd9da38a034587ca96a3b6a640
Nathan Cai <wenjuncai97@gmail.com>
2022-05-20 17:42:26 -0400

    added index server packagejson

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
-// build your server here and require it from index.js
+const express = require('express');
+const projectRouter = require('./project/router');
+const resourceRouter = require('./resource/router');
+const taskRouter = require('./task/router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/project', projectRouter);
+server.use('/api/resource', resourceRouter);
+server.use('/api/task', taskRouter);
+
+server.use('*', (req, res) => {
+  res.json({ api: 'up'})
+})
+
+module.exports = server;
